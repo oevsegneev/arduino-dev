2abc865ac1cd8aff4251c59701428d35bf1cfab0
oevsegneev <oleg.evsegneev@gmail.com>
2016-12-11 23:43:01 +0500

    [+] support for nrf24l01 library added

--- libs/SerialFlow/SerialFlow.cpp
+++ libs/SerialFlow/SerialFlow.cpp
@@ -1,27 +1,52 @@
 /*
   SerialFlow.cpp - Communication library with packages of values.
   Created by Oleg Evsegneev, September 11, 2012.
-  Last update 09.05.2016
+  Last update 13.11.2016
   Released into the public domain.
 */
 #include "SerialFlow.h"
 
-#ifdef FastSerial_h
+#if defined FastSerial_h
 SerialFlow::SerialFlow( FastSerial *serial ) {
+    _serial = serial;
+#elif defined __RF24_H__
+SerialFlow::SerialFlow( uint8_t cn, uint8_t csn ) {
+	_serial = new RF24(cn, csn);
 #else
 SerialFlow::SerialFlow( HardwareSerial *serial ) {
+    _serial = serial;
 #endif
     _escape = 0;
     _collecting = 0;
-    _serial = serial;
 }
 
-void SerialFlow::begin( long baud_rate ) {
+#ifdef __RF24_H__
+void SerialFlow::begin( uint64_t address1, uint64_t address2 ) {
+	_serial->begin();
+	_serial->setRetries( RF24_REPEAT_DELAY, RF24_REPEAT_COUNT );
+	_serial->setPayloadSize( RF24_PAYLOAD );
+	
+	_serial->openWritingPipe( address1 );
+	_serial->openReadingPipe( 1, address2 );
+	
+    _serial->startListening();	
+}
+#else
+void SerialFlow::begin( uint16_t baud_rate ) {
     _serial->begin(baud_rate);
 }
+#endif
 
-void SerialFlow::setPacketFormat( byte v_length, byte p_size, boolean separate ) {
+void SerialFlow::setPacketFormat( uint8_t v_length, uint8_t p_size, boolean separate ) {
     _separate = separate;
+    _p_size = p_size;
+    _v_length = v_length;
+    _vs_idx = 0;
+    _vr_idx = 0;
+}
+
+void SerialFlow::setPacketFormat( uint8_t v_length, uint8_t p_size ) {
+    _separate = 0;
     _p_size = p_size;
     _v_length = v_length;
     _vs_idx = 0;
@@ -35,29 +60,29 @@ void SerialFlow::setPacketValue( uint32_t value ) {
 }
 
 void SerialFlow::sendPacket() {
-    byte v;
-    _serial->write( 0x12 );
+    uint8_t v;
+    write( 0x12 );
     for( byte i=0; i<_vs_idx; i++ ){
         for( byte b=0; b<_v_length; b++ ){
             v = (_vs[i]>>(b<<3)) & 0xFF;
             if( v==0x12 || v==0x13 || v==0x7D || ( _separate && v==0x10 ))
-                _serial->write( 0x7D );
-            _serial->write( v );
+                write( 0x7D );
+            write( v );
         }
 
         // _separate values
         if( _separate && i < _vs_idx-1 )
-            _serial->write(0x10);
+            write(0x10);
     }
         
-    _serial->write( 0x13 );
+    write( 0x13 );
     _vs_idx = 0;
 }
 
 bool SerialFlow::receivePacket() {
-    byte c;
+    uint8_t c;
     while( _serial->available() ){
-        c = _serial->read();
+        c = read();
         if( _collecting )
             if( _escape ){
                 _vr_val[_cr_idx++] = c;
@@ -96,19 +121,31 @@ bool SerialFlow::receivePacket() {
     return 0;
 }
 
-uint32_t SerialFlow::getPacketValue( byte idx ) {
+uint32_t SerialFlow::getPacketValue( uint8_t idx ) {
     return _vr[idx];
 }
 
-void SerialFlow::write( byte v ) {
+void SerialFlow::write( uint8_t v ) {
+	#ifdef __RF24_H__
+    _serial->stopListening();
+    _serial->write( &v, 1);
+    _serial->startListening();	
+	#else
     _serial->write(v);
+	#endif
 }
 
-byte SerialFlow::read() {
+uint8_t SerialFlow::read() {
+	#ifdef __RF24_H__
+	uint8_t v;
+	_serial->read( &v, 1 );
+	return v;
+	#else
     return _serial->read();
+	#endif
 }
 
-uint32_t SerialFlow::_join_bytes(byte *bs) {
+uint32_t SerialFlow::_join_bytes(uint8_t *bs) {
     uint32_t v = 0;
     uint32_t x = 0;
     for( byte b=0; b<_v_length; b++ ){
@@ -116,4 +153,10 @@ uint32_t SerialFlow::_join_bytes(byte *bs) {
         v |= (x << (b<<3));
     }
     return v;
+}
+
+void SerialFlow::getHWInfo(){
+    #ifdef __RF24_H__
+    _serial->printDetails();
+    #endif
 }
--- libs/SerialFlow/SerialFlow.h
+++ libs/SerialFlow/SerialFlow.h
@@ -1,44 +1,55 @@
 /*
   SerialFlow.h - Communication library with packages of values.
   Created by Oleg Evsegneev, September, 2012.
-  Last update 09.05.2016
+  Last update 13.11.2016
   Released into the public domain.
-  Ver 0.7 (for Arduino)
+  Ver 0.8 (for Arduino)
 */
 #ifndef SerialFlow_h
 #define SerialFlow_h
 
 #include "Arduino.h"
 
-#ifdef FastSerial_h
-#include <FastSerial.h>
-#else
+// uncomment desired library
+//#include <FastSerial.h>
+//#include <RF24.h>
 #include <HardwareSerial.h>
-#endif
 
-#ifndef MAX_PACKET_SIZE
-#define MAX_PACKET_SIZE 128
+#define MAX_PACKET_SIZE 8
+
+#if defined __RF24_H__
+#define RF24_REPEAT_COUNT 1
+#define RF24_REPEAT_DELAY 15
+#define RF24_PAYLOAD 1
 #endif
 
 class SerialFlow {
 public:
-    #ifdef FastSerial_h
+    #if defined FastSerial_h
     SerialFlow( FastSerial *serial );
-    #else
-    SerialFlow( HardwareSerial *serial );
+    #elif defined __RF24_H__
+	SerialFlow( uint8_t cn, uint8_t csn );
+	#else
+	SerialFlow( HardwareSerial *serial );
     #endif
 
     /** Set data packet format
      *
      * @param v_length Length of value in bytes.
      * @param p_size Number of values.
+     * @param separate Flag to separate values by 0x10 char (optional).
      */
-    void setPacketFormat( byte v_length, byte p_size, boolean separate );
+    void setPacketFormat( uint8_t v_length, uint8_t p_size, boolean separate );
+    void setPacketFormat( uint8_t v_length, uint8_t p_size );
 
     /**  Initialize port
      * @param baud_rate Serial port baud rate.    
      */
-    void begin( long baud_rate );
+    #ifdef __RF24_H__
+	void begin( uint64_t address1, uint64_t address2 );
+	#else
+    void begin( uint16_t baud_rate );
+	#endif
 
     /**  Set value to data packet
      *
@@ -57,40 +68,44 @@ public:
     /**  Get received packet
      * @param idx Index of value from packet.    
      */
-    uint32_t getPacketValue( byte idx );
+    uint32_t getPacketValue( uint8_t idx );
 
     /**  Serial write
      * @param v Byte to send.    
      */
-    void write( byte v );
+    void write( uint8_t v );
 
     /**  Serial read
      */
-    byte read();
+    uint8_t read();
+
+    void getHWInfo();
 
 protected:
     #ifdef FastSerial_h
     FastSerial *_serial;
+    #elif defined __RF24_H__
+	RF24 *_serial;
     #else
     HardwareSerial *_serial;
     #endif
 
     boolean _separate;
-    byte _p_size;
-    byte _v_length;
+    uint8_t _p_size;
+    uint8_t _v_length;
 
     uint32_t _vs[MAX_PACKET_SIZE];
-    byte _vs_idx;
+    uint8_t _vs_idx;
 
     uint32_t _vr[MAX_PACKET_SIZE];
-    byte _vr_val[4];
-    byte _vr_idx;
-    byte _cr_idx;
+    uint8_t _vr_val[4];
+    uint8_t _vr_idx;
+    uint8_t _cr_idx;
     bool _escape;
     bool _collecting;
 
 
-    uint32_t _join_bytes(byte *bs);
+    uint32_t _join_bytes(uint8_t *bs);
 };
 
 #endif
